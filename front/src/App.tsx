b757687d314fbf94fe85055a1d00c067255c430d
Ryosuke Nanaumi <nanaumi@jreast.co.jp>
2025-11-16 04:33:10 +0900

    feat(TodaysPointView): mockjsonの作成と、今日のポイント画面への反映作成中

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 import { Header } from "./components/Header/Header";
 import TodaysPointView from "./components/TodaysPointView/TodaysPointView";
+// import exercises from "./mock/exercises.json";
+
+interface Exercise {
+  id: number;
+  name: string;
+  displayName: string;
+  baseUnit: string;
+  trainingIntensity: number;
+}
+interface TrainingLog {
+  userId: number;
+  exerciseId: number;
+  date: Date;
+  amount: number;
+}
 
 function App() {
+  // console.log("foo------------", exercises);
+  const userId = 1; // 仮で設定
   const [todayPoint, setTodayPoint] = useState(0);
+  const calcTodaypoint = (exercises: Exercise[]) => {
+    setTodayPoint(exercises[0].trainingIntensity);
+  };
+  useEffect(() => {
+    fetch("/mock/exercises.json")
+      .then((res) => res.json())
+      .then((res) => calcTodaypoint(res));
+  }, []);
   return (
     <div className={styles.appRoot}>
       <Header />
