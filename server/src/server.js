151f7e1c5579fb870e050bbb86b30cceb1bcd895
Ryosuke Nanaumi <nanaumi@jreast.co.jp>
2025-11-17 14:45:59 +0900

    server: feat(api): expressをserver.jsで立ち上げる設定を追加

---
+++ server/src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.use("/", (req, res) => {
+  res.send("fooが呼び出されました。");
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("server is running!");
+});
